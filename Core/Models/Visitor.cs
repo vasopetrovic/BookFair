3085438c24e08d6092f6fc06ecec3a5261182b53
ndjuricic1 <ndjuricic.design@gmail.com>
2026-09-20 18:37:02 +0200

    add Visitor class

--- Core/Models/Visitor.cs
+++ Core/Models/Visitor.cs
@@ -4,7 +4,26 @@ using System.Text;
 
 namespace Core.Models
 {
-    internal class Visitor
+
+    public enum VisitorType
     {
+        Regular,
+        VIP
+    }   
+    public class Visitor
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string IDCardNumber { get; set; } //zbog 0   
+        public DateTime MembershipYear { get; set; }
+        public VisitorType Status { get; set; }
+        public double AverageRating { get; set; }
+        List<string> WishList;
+
+
     }
 }
