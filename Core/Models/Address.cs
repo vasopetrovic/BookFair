f4e253273ff7ab6cbabe53a4076027aea37c40da
Vasilije Petrovic <vasilijepetrovic100904@gmail.com>
2026-09-20 18:46:22 +0200

    Add Book, Author and Address model classes

---
+++ Core/Models/Address.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+
+        public string Street { get; set; }
+        public int HouseNumber { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
