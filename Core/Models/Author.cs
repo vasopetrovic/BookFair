649bf80f4ab5ae5a08877888e198bc109b5de3b0
Vasilije Petrovic <vasilijepetrovic100904@gmail.com>
2026-09-21 20:21:46 +0200

    Added inheritance to Author

--- Core/Models/Author.cs
+++ Core/Models/Author.cs
@@ -4,15 +4,9 @@ using System.Text;
 
 namespace Core.Models
 {
-    public class Author
+    public class Author : Person
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public DateTime BirthDate { get; set; }
-        public Address Address { get; set; }
-        public string Telephone { get; set; }
-        public string Email { get; set; }
         public string IdCardNumber { get; set; } = string.Empty;
         public int ExperienceYears { get; set; }
         public List<Book> Books { get; set; } = new List<Book>();
