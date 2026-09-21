cd515b8e15d9a7c419245b18818a0e0c971a8986
ndjuricic1 <ndjuricic.design@gmail.com>
2026-09-21 20:12:24 +0200

    Created abstract class Person

---
+++ Core/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Models
+{
+    public abstract class Person
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Surname { get; set; } =string.Empty;
+        public DateTime Birthdate{ get; set; }
+        public Address Address { get; set; } = new Address();
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber {  get; set; } = string.Empty;
+
+    }
+}
