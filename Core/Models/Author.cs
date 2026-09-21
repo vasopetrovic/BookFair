638b98c84d2861842dff14c2448fb394b26f2307
Vasilije Petrovic <vasilijepetrovic100904@gmail.com>
2026-09-21 20:23:07 +0200

    Deleted id in class

--- Core/Models/Author.cs
+++ Core/Models/Author.cs
@@ -6,7 +6,6 @@ namespace Core.Models
 {
     public class Author : Person
     {
-        public int Id { get; set; }
         public string IdCardNumber { get; set; } = string.Empty;
         public int ExperienceYears { get; set; }
         public List<Book> Books { get; set; } = new List<Book>();
